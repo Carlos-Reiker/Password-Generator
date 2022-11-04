ce9b8222b373ec5369806c9542f2560847a7c130
Carlos Reiker <carlos.reiker@hotmail.com>
2022-11-04 20:19:41 -0300

    04/11/22

--- PIN-generator/pingenerator.js
+++
@@ -1,8 +0,0 @@
-function btnPin6Dig(){
-    const pin6 = Math.floor((Math.random() * 900000) + 100000);
-    document.getElementById('showPin').innerHTML= pin6;
-}
-function btnPin4Dig(){
-    const pin4 = Math.floor((Math.random() * 9000) + 1000);
-    document.getElementById('showPin').innerHTML= pin4;
-}
